6a985550cac6506186e5cdbc3315ca7a75b44c7f
Kah919 <kahyap13@gmail.com>
2020-07-12 16:47:29 -0400

    Fixed grid error where the following cards are already selected

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,7 @@
 import React, { useState } from 'react';
 
 export default (props) => {
-    const [select, setSelect] = useState(false)
-
-    const selected = (event) => {
-        props.cardIdx(props)
-        if(props.guess) {
-            alert(props.idx)
-            return props.idx
-        }
-        setSelect(!select)
-    }
-
-    const test = () => {
-        alert('right')
-    }
-
     return(
-        <img className={ select ? 'card-img eliminate' : 'card-img' } src={props.img} onClick={ selected } idx={props.idx} ></img>
+        <img className={ props.selected ? 'card-img eliminate' : 'card-img'} src={props.img} idx={props.idx} onClick={ () => props.selectCard(props.idx) } ></img>
     )
 }
