d2d27b2ec1f5fe96e3a6393a260ea9cd5942a7e3
Ted Zamarron <ted.zamarron@gmail.com>
2020-07-13 00:43:21 -0500

    completed end-screen css and functionality

--- src/components/GameEnd.js
+++ src/components/GameEnd.js
@@ -1,37 +1,21 @@
 import React from "react";
-import Calcifer from "../images/Calcifer.gif"
 
 export default function GameEnd(props) {
-  return (
-    // props.show
-    // ?<div>
-    //   <h1>{props.result.guess ? "Correct" : "Incorrect"} guess - Game Over!</h1>
-    //   <h3>Winner: {props.result.winner}</h3>
-    // </div>
-    // : <></>
-
-    <div className="winner-screen">
-      <svg>
-        <symbol id="s-text">
-          <text text-anchor="middle" x="50%" y="80%">
-            Congrats!
-          </text>
-        </symbol>
-
-        <g class="g-ants">
-          <use xlinkHref="#s-text" class="text-copy"></use>
-          <use xlinkHref="#s-text" class="text-copy"></use>
-          <use xlinkHref="#s-text" class="text-copy"></use>
-          <use xlinkHref="#s-text" class="text-copy"></use>
-          <use xlinkHref="#s-text" class="text-copy"></use>
-        </g>
-      </svg>
-      <div className="winner-div">
-      <img src={ Calcifer } />
-      <h2>Winner: Final Fantasy</h2>
-      <img src={ Calcifer } />
+  return props.show ? (
+    <div className="end-screen">
+      <h1>Game Over!</h1>
+      <div className="end-div">
+        <div className="username">{props.result.winner}</div>
+        <div className="message-div">
+          <span>
+            {props.result.guess
+              ? "Congrats! You Win!"
+              : "Better Luck Next Time..."}
+          </span>
+        </div>
       </div>
-      
     </div>
+  ) : (
+    <></>
   );
 }
