2069f872d396fd1385e68e939b3adc9f5c2321fb
Kah919 <kahyap13@gmail.com>
2020-07-13 11:29:58 -0400

    Add social media icons on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,14 @@ export default () => {
     return(
         <div className="footer-container">
             <ul>
+                <li className="fa fa-linkedin"></li>
                 <li><a href="https://www.linkedin.com/in/tzamarron/"> Ted </a></li>
                 <li><a href="https://www.linkedin.com/in/kah-m-yap/"> Kah </a></li>
                 <li><a href="https://www.linkedin.com/in/conswang/"> Constance </a></li>
             </ul>
 
             <ul className="footer-links">
+                <li className="fa fa-github"></li>
                 <li><a href="https://github.com/conswang/guess-who-infinite-client"> client </a></li>
                 <li><a href="https://github.com/conswang/guess-who-infinite-server"> server </a></li>
                 <li><a href="https://www.mintbean.io/"> mintbean </a></li>
