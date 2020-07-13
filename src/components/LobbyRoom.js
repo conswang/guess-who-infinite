4d2f821613c704edb9059be646ce5cc0f5506610
Ted Zamarron <ted.zamarron@gmail.com>
2020-07-12 21:50:09 -0500

    added animation to reply

--- src/components/LobbyRoom.js
+++ src/components/LobbyRoom.js
@@ -1,20 +1,43 @@
-import React, { useEffect } from 'react';
-import { socket } from '../App';
+import React, { useEffect } from "react";
+import { socket } from "../App";
 
 export default (props) => {
-
-    useEffect(() => {
-        socket.on('startedGame', () => {
-            props.callback('select')
-        });
+  useEffect(() => {
+    socket.on("startedGame", () => {
+      props.callback("select");
     });
+  });
 
-    return(
-        <div className="lobby-container">
-            <p> Type join code: </p>
-            <h1> { props.code }  </h1>
-            <div class="loader"></div>
-            <p> Waiting for player 2... </p>
-        </div>
-    )
-}
+  return (
+    <div className="lobby-container">
+      <p> Type join code: </p>
+      <h1> {props.code} </h1>
+      <div class="loader"></div>
+      <div class="loading-container">
+        <div class="loading__letter">W</div>
+        <div class="loading__letter">a</div>
+        <div class="loading__letter">i</div>
+        <div class="loading__letter">t</div>
+        <div class="loading__letter">i</div>
+        <div class="loading__letter">n</div>
+        <div class="loading__letter">g</div>
+        <div class="loading__letter">&nbsp;</div>
+        <div class="loading__letter">f</div>
+        <div class="loading__letter">o</div>
+        <div class="loading__letter">r</div>
+        <div class="loading__letter">&nbsp;</div>
+        <div class="loading__letter">p</div>
+        <div class="loading__letter">l</div>
+        <div class="loading__letter">a</div>
+        <div class="loading__letter">y</div>
+        <div class="loading__letter">e</div>
+        <div class="loading__letter">r</div>
+        <div class="loading__letter">&nbsp;</div>
+        <div class="loading__letter">2</div>
+        <div class="loading__letter">.</div>
+        <div class="loading__letter">.</div>
+        <div class="loading__letter">.</div>
+      </div>
+    </div>
+  );
+};
